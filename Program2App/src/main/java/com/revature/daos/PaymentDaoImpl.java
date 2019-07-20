8b3f82569e3357742241e872faf07e4669868879
Saidnajibullah <najibullahqubadi@gmail.com>
2019-07-20 17:16:40 -0400

    dogs, payments, and insurance tables were submitted

---
+++ Program2App/src/main/java/com/revature/daos/PaymentDaoImpl.java
@@ -0,0 +1,53 @@
+package com.revature.daos;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Component;
+
+import com.revature.beans.Insurance;
+import com.revature.beans.Payments;
+import com.revature.beans.User;
+import com.revature.util.SessionFactoryUtil;
+
+@Component
+public class PaymentDaoImpl implements PaymentDao {
+	private static SessionFactory sf = SessionFactoryUtil.getSessionFactory();
+	
+
+	@Override
+	public User makePayment(User user, int id) {
+		Session sess = sf.openSession();
+		user = sess.get(User.class, id);
+		sess.close();
+		
+		return user;
+
+	}
+
+	@Override
+	public List<Payments> seeAllPayments() {
+		Session sess = sf.openSession();
+		@SuppressWarnings("deprecation")
+		Criteria crit = sess.createCriteria(Payments.class);
+		@SuppressWarnings("unchecked")
+		List<Payments> result = crit.list();
+		sess.close();
+		return result;
+	}
+	
+    public void submitPayment(Payments payment) {
+		
+		Session sess = sf.openSession();
+		Transaction tx = sess.beginTransaction();
+		sess.save(payment);
+		tx.commit();
+		sess.clear();
+		
+
+	}
+
+}
